c60c6126977b3605bbc56f44548664074d62ccb3
Danil <Skoryak>
2023-03-05 00:17:31 +0100

    Skoryak_book

---
+++ Skoryak_oop/Skoryak_book.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, name, author, year_publication):
+        self.name = name
+        self.author = author
+        self.year_publication = year_publication
+
+    def get_data_book(self):
+        print(f'\n\tName: {self.name}   Author: {self.author}   Year of publication: {self.year_publication}\n')
+
+    def get_data_age(self):
+        try:
+            if self.year_publication <= 0:
+                raise ValueError
+            print(f'\tThis book is {2023 - self.year_publication} years old\n')
+        except ValueError:
+            print('Incorrect year of publication\n')
+        except TypeError:
+            print('Use only numbers for year of publication\n')
+            
